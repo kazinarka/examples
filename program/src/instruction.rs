1f1970a03a76eae9c1905270b2555796fd187c89
Illia Fedotov <illia.fedotov@ideasoft.io>
2022-10-12 12:22:28 +0300

    Initial commit

---
+++ program/src/instruction.rs
@@ -0,0 +1,8 @@
+use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+
+#[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
+pub enum ExampleInstruction {
+    SayHello,
+}
+
+impl ExampleInstruction {}
---
+++ program/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod entrypoint;
+pub mod error;
+pub mod instruction;
+pub mod processor;
+
+solana_program::declare_id!("4T7xMGLUSPhpnAou6TiZDB9XCDHgGx1u7hDbGWdXnfmG");
